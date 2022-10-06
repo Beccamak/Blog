bbd5f41620708aba94d56b32dbb695fe3d886d77
Beccamak <makindeoluwafunmilayo038@gmail.com>
2022-10-06 08:05:11 -0700

    initial commit my-blog

---
+++ src/pages/page-3.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+
+import Layout from "../components/layout"
+
+const SecondPage = () => (
+  <Layout>
+    <h1>Hi from the third page</h1>
+ 
+   
+  </Layout>
+)
+
+
+export default SecondPage
